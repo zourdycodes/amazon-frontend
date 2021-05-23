671dafc0113100621772bb97766a31c17bfa0067
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-24 00:35:32 +0700

    clean up: adding Fake Store API and make a card components for the products

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,43 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 function Banner() {
   return (
-    <div>
-      <h1>gotta be here</h1>
+    <div className="relative">
+      {/* 
+      1.  bg-gradient-to-t ==> from bottom to top
+      */}
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20 " />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img
+            loading="lazy"
+            src="https://links.papareact.com/gi1"
+            alt="carousel-banner"
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="https://links.papareact.com/6ff"
+            alt="carousel-banner"
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="https://links.papareact.com/7ma"
+            alt="carousel-banner"
+          />
+        </div>
+      </Carousel>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ function Header() {
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
-            objectFit="cont ain"
+            objectFit="contain"
             className="cursor-pointer"
           />
         </div>
