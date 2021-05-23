a2de60f2c6f4c8384c24ca6855cae5f1f662f623
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-23 21:12:40 +0700

    adding react-responsive-carousel

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+function Banner() {
+  return (
+    <div>
+      <h1>gotta be here</h1>
+    </div>
+  );
+}
+
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -10,6 +11,13 @@ export default function Home() {
 
       {/* HEADER */}
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+
+        {/* Product Feed */}
+      </main>
     </div>
   );
 }
