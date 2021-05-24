0e549741a29acd943f5dc60a5cf97636f8ea290c
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-24 09:40:28 +0700

    add: Google Tag Manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,19 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import "../styles/globals.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-N5PWSZ6" });
+  }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
